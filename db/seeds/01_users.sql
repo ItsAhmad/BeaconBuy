242e46e0b2463708643d8ae2a2b5d2ac28c7aa73
Shanel Beebe <shanel.beebe@publicisna.com>
2024-04-28 17:25:04 -0600

    add login functionality, user seeds, add header partial and login.ejs.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,13 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, username, password, email) VALUES ('Kylie Jenner', 'kyliejay', 'password', 'kyliejay@kyliecosmetics.com');
-INSERT INTO users (name, username, password, email) VALUES ('Oprah Winfrey', 'yesthatoprah', 'password', 'oprah@harpostudios.com');
-INSERT INTO users (name, username, password, email) VALUES ('Jeff Bezos', 'amazonking', 'password', 'jeffyb@amazon.com');
+INSERT INTO users (name, username, password, email) VALUES ('Kylie Jenner', 'kyliejay', 'buyer', 'kyliejay@kyliecosmetics.com');
+INSERT INTO users (name, username, password, email) VALUES ('Oprah Winfrey', 'yesthatoprah', 'seller', 'oprah@harpostudios.com');
+INSERT INTO users (name, username, password, email) VALUES ('Jeff Bezos', 'amazonking', 'beaconbuypassword', 'jeffreypbezos@amazon.com');
+INSERT INTO users (name, username, password, email) VALUES ('Taylor Swift', 'taytay89', 'beaconbuypassword', 'itsmehi@taylorswift.com');
+INSERT INTO users (name, username, password, email) VALUES ('Beyonce', 'queenbey', 'beaconbuypassword', 'mscarter@beyonce.com');
+INSERT INTO users (name, username, password, email) VALUES ('Rihanna', 'robin_riri', 'beaconbuypassword', 'rihanna@fentybeauty.com');
+INSERT INTO users (name, username, password, email) VALUES ('Sheryl Sandberg', 'bossgirl', 'beaconbuypassword', 'sheryl@leanin.org');
+INSERT INTO users (name, username, password, email) VALUES ('Harry Sussex', 'princehaz', 'beaconbuypassword', 'dukeofsussex@royal.uk');
+INSERT INTO users (name, username, password, email) VALUES ('George Clooney', 'silverfox61', 'beaconbuypassword', 'george_clooney@gmail.com');
+INSERT INTO users (name, username, password, email) VALUES ('Madonna', 'thematerialgirl', 'beaconbuypassword', 'maddonnalouise@hotmail.com');
+INSERT INTO users (name, username, password, email) VALUES ('Lionel Messi', 'la_pulga_atomica', 'beaconbuypassword', 'lmessi@intermiamicf.com');
+INSERT INTO users (name, username, password, email) VALUES ('Victoria Beckham', 'poshandbecks', 'beaconbuypassword', 'vbeckham@victoriabeckham.com');
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const cookieSession = require("cookie-session");
+const userDatabase = require("../db/queries/users.js");
 
 router.get("/login", (req, res) => {
-  console.log(res)
+  const username = req.session['username']
+
   res.render("login");
 });
 
