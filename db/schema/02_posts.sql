6bac0e5eb1bdd7f232c861cded20ae94cf24e228
Monica Palko <m.a.palko07@gmail.com>
2024-05-02 23:10:16 -0400

    edited posts and images

--- db/schema/02_posts.sql
+++ db/schema/02_posts.sql
@@ -1,8 +1,10 @@
+DROP TABLE if exists posts cascade;
+
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   boat_title VARCHAR(100) NOT NULL,
   boat_description TEXT NOT NULL,
-  boat_price DECIMAL(10, 2)
+  boat_price DECIMAL(10, 2),
   image_url VARCHAR(255)
 )
