e00ecdaf8e2eb2822ecfc525d3eb53a170bb185f
Shanel Beebe <shanel.beebe@publicisna.com>
2024-05-03 20:07:58 -0600

    start adding single user-post page functionality

--- routes/add-post.js
+++ routes/add-post.js
@@ -3,6 +3,14 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const db = require("../db/connection");
+const cookieSession = require("cookie-session");
+router.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -36,18 +44,16 @@ router.post("/", upload.single("image"), (req, res) => {
   const image_url = req.file.path;
   const user_id = req.session.user_data[0].id;
 
-  const sql =
-    "INSERT INTO posts (user_id, boat_title, boat_description, boat_price, image_url) VALUES ($1,$2,$3,$4,$5);";
-  db.query(sql, [user_id, boat_title, boat_description, boat_price, image_url])
+  const query1 =
+    "INSERT INTO posts (user_id, boat_title, boat_description, boat_price, image_url) VALUES ($1,$2,$3,$4,$5) RETURNING id;";
+  db.query(query1, [user_id, boat_title, boat_description, boat_price, image_url])
     .then((data) => {
-     return res.send(image_url)
+      req.session.id = data.rows[0].id;
+      return res.send(image_url);
     })
     .catch((err) => {
       return res.status(500).send(err);
     })
-
-  res.redirect('/')
-
 });
 
 module.exports = router;
