990091a5fb3ff1e239e837af6b6f4a1b607a01ec
Shanel Beebe <shanel.beebe@publicisna.com>
2024-05-04 10:26:31 -0600

    updates to user-post page and route

--- routes/post1.js
+++ routes/post1.js
@@ -5,7 +5,14 @@ const router = express.Router();
 
 // GET /post/1 - Render the post 1 details page
 router.get('/', (req, res) => {
-    res.render('post1'); // Render the post1.ejs template
+    const userData = req.headers.cookie;
+    console.log(req.session.id);
+
+    const templateVars = {
+      userData,
+      // id : req.session.id
+    };
+    res.render('post1', templateVars); // Render the post1.ejs template
 });
 
 module.exports = router;
